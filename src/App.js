302de8dd0515592c032fea2ee6e7a4bc141422a5
aidarous <aidarous12@gmail.com>
2020-07-29 16:54:47 -0400

    imported quotes into App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home';
+import Quotes from './components/Quotes'
 
 
 function App() {

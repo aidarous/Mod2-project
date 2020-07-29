3ad2a47953513ad36ca3a80cdf78cbe76773afc4
aidarous <aidarous12@gmail.com>
2020-07-29 11:54:29 -0400

    imported Home.js into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import './App.css';
+import './App.css'
+import Home from './components/Home';
 
 
 function App() {

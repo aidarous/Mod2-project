dc5e0073d3c2613623e00854de13ec88d4dd1727
aidarous <aidarous12@gmail.com>
2020-07-29 11:42:24 -0400

    changed boilerplate class to Home class

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class componentName extends Component {
+class Home extends Component {
     render() {
         return (
             <div>

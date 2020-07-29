764c1a2351cf9b8cf1e23a9317f5df437c693f07
aidarous <aidarous12@gmail.com>
2020-07-29 10:41:33 -0400

    imported router library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (

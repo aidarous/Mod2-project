b2ac70be199b766c5663121c653f2947fc087efa
aidarous <aidarous12@gmail.com>
2020-07-29 11:09:59 -0400

    created router tags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
-import { Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       
-    </div>
+      </div>
+
+    </Router>
+    
   );
 }
 

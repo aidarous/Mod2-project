a32db756121ca18c8301ab801296e90ac652a87f
aidarous <aidarous12@gmail.com>
2020-07-29 13:56:47 -0400

    link made into home button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <navbar>
-          <Link></Link>
+          <Link to="Home">Home </Link>
         </navbar>
       
       </div>

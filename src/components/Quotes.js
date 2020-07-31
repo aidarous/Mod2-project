ba0277acc484b1e8a9b7b45a8de137bc16034af5
aidarous <aidarous12@gmail.com>
2020-07-31 00:40:03 -0400

    created constructor in Quotes.js

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -9,6 +9,9 @@ import axios from 'axios';
 // * display it for the user in an oragnized/filtered manner
 
 class Quotes extends Component {
+    constructor(){
+        
+    }
     render() {
         return (
             <div>

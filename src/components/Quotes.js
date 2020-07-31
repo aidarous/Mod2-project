27be8f3bb9b3ef710804025fdac9820694e98145
aidarous <aidarous12@gmail.com>
2020-07-31 00:19:57 -0400

    wrote some pseudocode

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 
-// Pseudo code 
+// * Pseudo code 
+ 
+// * This file will include code to display famous quotes to inspire writers
+// * First access the api and fetch its data using async functions
+// * display it for the user in an oragnized/filtered manner
 
 class Quotes extends Component {
     render() {

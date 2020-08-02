b3958d113d191a55f0e393103de00e3b60e99855
aidarous <aidarous12@gmail.com>
2020-08-02 09:27:03 -0400

    rendered joke1

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -3,11 +3,33 @@ import axios from 'axios';
 
 
 class Jokes extends Component {
-    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+    async getJokes() {
+            const result = await axios.get("https://official-joke-api.appspot.com/random_ten")
+            const randomSetup = result.data[0].setup
+            const punchlineOne = result.data[0].punchline
+            
+            
+            this.setState({
+                setup1: randomSetup,
+                punchline1: punchlineOne
+            })
+        }
+    componentDidMount () {
+        this.getJokes();
+    }
     render() {
         return (
             <div>
                 <h1>Jokes</h1>
+                <h2>{this.state.setup1}</h2>
+                <p> {this.state.punchline1}</p>
                
 
             </div>

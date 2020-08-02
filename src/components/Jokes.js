0f6ba76158751ba14a75776ce40353278d11af91
aidarous <aidarous12@gmail.com>
2020-08-02 09:33:44 -0400

    setup2 and puncline2 rendered

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -14,11 +14,15 @@ class Jokes extends Component {
             const result = await axios.get("https://official-joke-api.appspot.com/random_ten")
             const randomSetup = result.data[0].setup
             const punchlineOne = result.data[0].punchline
+            const randomSetupTwo = result.data[1].setup
+            const punchlineTwo = result.data[1].punchline
             
             
             this.setState({
                 setup1: randomSetup,
-                punchline1: punchlineOne
+                punchline1: punchlineOne,
+                setup2: randomSetupTwo,
+                punchline2: punchlineTwo,
             })
         }
     componentDidMount () {
@@ -30,6 +34,8 @@ class Jokes extends Component {
                 <h1>Jokes</h1>
                 <h2>{this.state.setup1}</h2>
                 <p> {this.state.punchline1}</p>
+                <h2>{this.state.setup2}</h2>
+                <p> {this.state.punchline2}</p>
                
 
             </div>
